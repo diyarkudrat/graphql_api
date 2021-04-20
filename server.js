95a1c8e0ffa008022b1c9dbf01397277ab4cc041
Diyar Kudrat <dkudrat@gmail.com>
2021-04-20 11:45:33 -0700

    added resolver to retrieve temp and description of specified zipcode's weather

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const fetch = require('node-fetch');
 require('dotenv').config();
 
 // API Key
@@ -24,7 +25,16 @@ type Query {
 
 // Resolvers
 const root = {
-
+    getWeather: async ({ zip }) => {
+        const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+        const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}`;
+        const res = fetch(url);
+        const json = await res.json();
+        const temperature = json.main.temp;
+        const description = json.weather[0].description;
+        
+        return { temperature, description }
+    }
 }
 
 const app = express();
